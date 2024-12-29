8b7a1724ea5380e41d5e1a4a6a81e54cc6c4f360
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-29 14:52:58 -0600

    Added navigation bar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ResumeComponent } from './resume/resume.component';
 
 export const routes: Routes = [];
