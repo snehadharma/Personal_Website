3195cc78ae7dad6f04195b46b5928944b703aa6f
Sneha Dharmalingam <sneha.dharma112@gmail.com>
2024-12-29 15:17:51 -0600

    Added functionality to menu bar.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,4 +3,18 @@ import { ContactComponent } from './contact/contact.component';
 import { ProjectsComponent } from './projects/projects.component';
 import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path : 'contact-component',
+        component : ContactComponent
+    }, 
+    {
+        path : 'projects-component',
+        component : ProjectsComponent
+    }, 
+    {
+        path : 'resume-component',
+        component : ResumeComponent
+    }, 
+
+];
